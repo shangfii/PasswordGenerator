398961e2551edf36bd4c4127b8d4e75bf7ab6485
shangfii <84684636+shangfii@users.noreply.github.com>
2022-03-14 03:48:59 -0700

    Update script.js
    setting up arrys

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,13 @@
 // Variables to be used in our password prompt collection function
- var passLength = 8;
- var choiceArr = [];
+ var passwordLength = 8;
+ var choice = [];
  
- // Array's of all lowercase, and uppercase letters, as wekll as numbers, and special characters.
- var lowercaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l", "m","n","o","p","q","r","s","t","u","v","w","x","y","z" ]
- var UppercaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
- var numberArr = [1,2,3,4,5,6,7,8,9,0]
- var specCharacterArr = [' ', '!','"','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[', ' \ ' ,']','^','_','`','{','|','}','~']
+ // Arrys of choices : lowercase, and uppercase letters, numbers, and special characters.
+
+ var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l", "m","n","o","p","q","r","s","t","u","v","w","x","y","z" ]
+ var Uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
+ var number = [1,2,3,4,5,6,7,8,9,0]
+ var specCharacter = [' ', '!','"','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[', ' \ ' ,']','^','_','`','{','|','}','~']
  
  // Assignment Code
  var generateBtn = document.querySelector("#generate");
@@ -15,6 +16,7 @@
  generateBtn.addEventListener("click", writePassword);
  
  // Write password to the #password input
+
  function writePassword() {
    choiceArr=[]
    var correctPrompts= collectPrompts();
@@ -32,13 +34,13 @@
 
  // Generate password function
  function generatePassword() {
-   console.log(choiceArr);
+   console.log(choice);
  
    // generates according to prompts selected 
    var password= "";
-   for(var i = 0; i < passLength; i++) {
-     var randomIndex = Math.floor(Math.random() * choiceArr.length);
-     password = password+choiceArr[randomIndex]; 
+   for(var i = 0; i < passwordLength; i++) {
+     var randomIndex = Math.floor(Math.random() * choice.length);
+     password = password+choice[randomIndex]; 
    }
    return password;
    
@@ -46,7 +48,7 @@
  
  // Creating a prompt allowing us to choose our character amount.
    function collectPrompts() {
-   passLength = parseInt(prompt("Password length can be between 8-128 characters.\n Please choose wisely. "));
+   passwordLength = parseInt(prompt("Password length can be between 8-128 characters.\n Please choose wisely. "));
  
    if (isNaN(passLength) || passLength <8 || passLength > 128) {
      alert("You have created the password of DOOM.\n Please retry i'm begging you. Enter a numeric value between 8-128. ");
@@ -55,42 +57,42 @@
 //  Now we decide what kind of characters we want using confirm and if/else statements
 // if true we will select a random character from the specified variable
    if (confirm("Marvelous. Shall this password contain lowercase letters?")){
-     choiceArr = choiceArr.concat(lowercaseArr);
+     choice = choice.concat(lowercase);
    }
   //  if false(cancel selection) then it will return nothing
-   else if (choiceArr) {
+   else if (choice) {
   } 
   else {
-    (choiceArr)
+    (choice)
     return false;
   }
    
    if (confirm("Alright then. Any Upppercase letters?")){
-     choiceArr = choiceArr.concat(UppercaseArr);
+     choice = choice.concat(UppercaseArr);
    }
-   else if (choiceArr){
+   else if (choice){
    }
-   else{ (choiceArr)
+   else{ (choice)
     return false;
   
    }
  
    if (confirm("Can do. How about numbers?")){
-       choiceArr = choiceArr.concat(numberArr);
+       choiceArr = choice.concat(number);
      }
-     else if (choiceArr){
+     else if (choice){
      }
-    else{ (choiceArr)
+    else{ (choice)
       return false;
     }
 
  
   if (confirm("And finally, would you like any special characters?")){
-         choiceArr = choiceArr.concat(specCharacterArr);
+         choiceArr = choice.concat(specCharacter);
       }
-      else if (choiceArr){
+      else if (choice){
       }
-     else{ (choiceArr)
+     else{ (choice)
       return false;}
  
        return true; 
