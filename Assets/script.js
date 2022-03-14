fac93edc025a9442a35ceedfc7b5d616e23f0e10
shangfii <84684636+shangfii@users.noreply.github.com>
2022-03-14 03:44:41 -0700

    Update script.js
    new code

--- Assets/script.js
+++ Assets/script.js
@@ -1,105 +1,97 @@
 // Variables to be used in our password prompt collection function
-var passLength = 10;
-var choiceList = [];
-
-// List of all variables to chose from 
-
-var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l", "m","n","o","p","q","r","s","t","u","v","w","x","y","z" ]
-var Uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
-var number = [1,2,3,4,5,6,7,8,9,0]
-var specCharacter = [' ', '!','"','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[', ' \ ' ,']','^','_','`','{','|','}','~']
-
-// Assignment this Code to the document
-
-var generateBtn = document.querySelector("#generate");
-
-// Add event listener to generate password button
-
-generateBtn.addEventListener("click", writePassword);
-
-// Write password to the #password input
-
-function writePassword() {
-  choice =[]
-  var correctPrompts= collectPrompts();
-  if(correctPrompts) {
-  var newPassword = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = newPassword;
+ var passLength = 8;
+ var choiceArr = [];
  
- } 
-
-else{
-    passwordText.value="";
-  }
-}
-
-// Generate password function
-function generatePassword() {
-  console.log(choice);
-
-  // generates according to prompts selected 
-
-  var password= "";
-  for(var i = 0; i < passLength; i++) {
-    var randomIndex = Math.floor(Math.random() * choice.length);
-    password = password+choice[randomIndex]; 
-  }
-  return password;
+ // Array's of all lowercase, and uppercase letters, as wekll as numbers, and special characters.
+ var lowercaseArr = ["a","b","c","d","e","f","g","h","i","j","k","l", "m","n","o","p","q","r","s","t","u","v","w","x","y","z" ]
+ var UppercaseArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
+ var numberArr = [1,2,3,4,5,6,7,8,9,0]
+ var specCharacterArr = [' ', '!','"','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[', ' \ ' ,']','^','_','`','{','|','}','~']
+ 
+ // Assignment Code
+ var generateBtn = document.querySelector("#generate");
+ 
+ // Add event listener to generate button
+ generateBtn.addEventListener("click", writePassword);
+ 
+ // Write password to the #password input
+ function writePassword() {
+   choiceArr=[]
+   var correctPrompts= collectPrompts();
+   if(correctPrompts) {
+   var newPassword = generatePassword();
+   var passwordText = document.querySelector("#password");
+   passwordText.value = newPassword;
   
-}
+  } 
 
-// Creating a prompt allowing us to choose our character amount.
-
-  function collectPrompts() {
-  passLength = parseInt(prompt("Your Password must be between 8-128 characters.\n Enter the number of characters "));
-
-  if (isNaN(passLength) || passLength <8 || passLength > 128) {
-    alert(" Your Password must be between 8-128 characters.\n Enter the number of characters  ");
-    return false;
-  }
-//   characters confirmation and if/else statements
-// if true we will select a random character from the specified variable
-
-  if (confirm("Great! Should Password include lowercase letters?")){
-    choice = choice.concat(lowercase);
-  }
- //  if false(cancel selection) then it will return nothing
-
-  else if (choice) {
- } 
- else {
-   (choice)
-   return false;
+ else{
+     passwordText.value="";
+   }
  }
-  
-  if (confirm("Cool!. Include Upppercase letters?")){
-    choice = choice.concat(Uppercase);
-  }
-  else if (choice){
-  }
-  else{ (choice)
-   return false;
- 
-  }
 
-  if (confirm("Perfect!. Should therw be numbers?")){
-      choice = choice.concat(number);
-    }
-    else if (choice){
-    }
-   else{ (choice)
+ // Generate password function
+ function generatePassword() {
+   console.log(choiceArr);
+ 
+   // generates according to prompts selected 
+   var password= "";
+   for(var i = 0; i < passLength; i++) {
+     var randomIndex = Math.floor(Math.random() * choiceArr.length);
+     password = password+choiceArr[randomIndex]; 
+   }
+   return password;
+   
+ }
+ 
+ // Creating a prompt allowing us to choose our character amount.
+   function collectPrompts() {
+   passLength = parseInt(prompt("Password length can be between 8-128 characters.\n Please choose wisely. "));
+ 
+   if (isNaN(passLength) || passLength <8 || passLength > 128) {
+     alert("You have created the password of DOOM.\n Please retry i'm begging you. Enter a numeric value between 8-128. ");
      return false;
    }
-
-
- if (confirm("Lastly!, winclude characters?")){
-        choice = choice.concat(specCharacter);
+//  Now we decide what kind of characters we want using confirm and if/else statements
+// if true we will select a random character from the specified variable
+   if (confirm("Marvelous. Shall this password contain lowercase letters?")){
+     choiceArr = choiceArr.concat(lowercaseArr);
+   }
+  //  if false(cancel selection) then it will return nothing
+   else if (choiceArr) {
+  } 
+  else {
+    (choiceArr)
+    return false;
+  }
+   
+   if (confirm("Alright then. Any Upppercase letters?")){
+     choiceArr = choiceArr.concat(UppercaseArr);
+   }
+   else if (choiceArr){
+   }
+   else{ (choiceArr)
+    return false;
+  
+   }
+ 
+   if (confirm("Can do. How about numbers?")){
+       choiceArr = choiceArr.concat(numberArr);
      }
-     else if (choice){
+     else if (choiceArr){
      }
-    else{ (choice)
-     return false;}
-
-      return true; 
+    else{ (choiceArr)
+      return false;
     }
+
+ 
+  if (confirm("And finally, would you like any special characters?")){
+         choiceArr = choiceArr.concat(specCharacterArr);
+      }
+      else if (choiceArr){
+      }
+     else{ (choiceArr)
+      return false;}
+ 
+       return true; 
+     }
