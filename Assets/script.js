817755380868e50c7024461440e61f20c9a8ff8a
shangfii <84684636+shangfii@users.noreply.github.com>
2022-03-20 14:25:35 -0700

    Update script.js

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,7 @@
  var passLength = 8;
  var choice = [];
  
- // Array's of cases to chose from (lowercase, uppercase letters and numbers and special characters)
+ // Array of cases to chose from (lowercase, uppercase letters and numbers and special characters)
  var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l", "m","n","o","p","q","r","s","t","u","v","w","x","y","z" ]
  var Uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
  var number= [1,2,3,4,5,6,7,8,9,0]
